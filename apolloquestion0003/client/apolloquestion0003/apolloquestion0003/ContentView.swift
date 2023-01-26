b61d97c3cdb4e30a2406d0ad26230f931cc3bf7d
michael.wilson <michael.wilson@hopin.to>
2023-01-26 11:52:07 -0600

    Update question 3 with a possible mitigation, and that mitigation's problems

--- apolloquestion0003/client/apolloquestion0003/apolloquestion0003/ContentView.swift
+++ apolloquestion0003/client/apolloquestion0003/apolloquestion0003/ContentView.swift
@@ -59,9 +59,63 @@ class AppController: ObservableObject {
         }
     }
 
+    class ApolloCancellableAndSwiftContinuationWrapper<ContinuationType, GraphQLResultType> {
+        var isCancelled = false
+        public internal(set) var apolloCancellable: Apollo.Cancellable? = nil
+
+        func cancel() {
+            apolloCancellable?.cancel()
+            isCancelled = true
+        }
+
+        func setApolloCancellable(apolloCancellable: Apollo.Cancellable, continuation: CheckedContinuation<ContinuationType, Never>) {
+            if isCancelled {
+                // Avoid leaking the continuation?  This should only be called if the task is cancelled though, so would the continuation be taken care of?
+                apolloCancellable.cancel()
+            }
+            else {
+                self.apolloCancellable = apolloCancellable
+            }
+        }
+
+        func handleResult(result: GraphQLResultType) {
+            // Call resume on the continuation with whatever is in the result...
+        }
+
+        deinit {
+            // Avoid leaking the continuation
+        }
+    }
+
+    @MainActor
+    func asyncFetchWithWrapper() async -> String {
+        let fetchCancellable = ApolloCancellableAndSwiftContinuationWrapper<String, Result<GraphQLResult<ThingByIdQuery.Data>, any Error>>()
+        return await withTaskCancellationHandler { [fetchCancellable] in
+            fetchCancellable.cancel()
+        } operation: {
+            return await withCheckedContinuation { continuation in
+                do {
+                    try Task.checkCancellation()
+                    if let apolloCancellable = self.apolloGraphQLConn?.fetch(
+                        query: SwiftCodeGeneratedByApollo.ThingByIdQuery(
+                            id: "does_not_matter_1"), resultHandler: { result in
+                                fetchCancellable.handleResult(result: result)
+                            }) {
+                        // TODO: WHAT IF WE GET A RESULT IN THE resultHandler ABOVE, BEFORE WE SET THIS?  WE COULD CACHE THE RESULT IN THE ApolloCancellableAndSwiftContinuationWrapper AND WAIT FOR THE CONTINUATION, BUT THAT'S GETTING VERY CONVOLUTED... IS THERE ANY EASIER WAY TO STRUCTURE THIS?
+                        fetchCancellable.setApolloCancellable(apolloCancellable: apolloCancellable, continuation: continuation)
+                    }
+                    else {
+                    }
+                }
+                catch {}
+            }
+        }
+    }
+
     @MainActor
     func doFetch() async {
-        fetchResult = await asyncFetch()
+        //fetchResult = await asyncFetch()
+        fetchResult = await asyncFetchWithWrapper()
     }
 
     func toggleViewWithTask() {
